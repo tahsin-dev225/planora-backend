41aa09c0874543b1ef5b9a12e2a64337e83c1cb8
tahsin-dev225 <ahmedtasin225@gmail.com>
2026-03-25 22:20:35 +0600

    stats for dashboard

--- src/app/module/stats/stats.service.ts
+++ src/app/module/stats/stats.service.ts
@@ -1,5 +1,5 @@
 import status from "http-status";
-import { Role } from "../../../generated/prisma/enums";
+import { ParticipantStatus, PaymentStatus, Role } from "../../../generated/prisma/enums";
 import AppError from "../../errorHalpers/AppError";
 import { IRequestUser } from "../../interface/requestUser.interface";
 import { prisma } from "../../lib/prisma";
@@ -29,38 +29,72 @@ const getDashboardStatsData = async (user: IRequestUser) => {
 const getSuperAdminStatsData = async () => {
   const totalUsers = await prisma.user.count();
   const totalEvents = await prisma.event.count();
-  const totalParticipants = await prisma.participant.count();
-  const totalPayments = await prisma.payment.count();
+  const totalParticipants = await prisma.participant.count({
+    where: {
+      status: ParticipantStatus.APPROVED
+    }
+  });
+  const totalRevenue = await prisma.payment.aggregate({
+    _sum: {
+      amount: true
+    },
+    where: {
+      status: PaymentStatus.SUCCESS
+    }
+  });
 
   return {
     totalUsers,
     totalEvents,
     totalParticipants,
-    totalPayments,
+    totalRevenue,
   };
 }
 
 const getAdminStatsData = async () => {
   const totalEvents = await prisma.event.count();
-  const totalParticipants = await prisma.participant.count();
-  const totalPayments = await prisma.payment.count();
+  const totalParticipants = await prisma.participant.count(
+    {
+      where: {
+        status: ParticipantStatus.APPROVED
+      }
+    }
+  );
+  const toatalUsers = await prisma.user.count();
+  const totalRevenue = await prisma.payment.aggregate({
+    _sum: {
+      amount: true
+    },
+    where: {
+      status: PaymentStatus.SUCCESS
+    }
+  });
 
   return {
     totalEvents,
+    toatalUsers,
     totalParticipants,
-    totalPayments,
+    totalRevenue,
   };
 }
 
 const getUserStatsData = async (user: IRequestUser) => {
-  const totalEvents = await prisma.event.count();
-  const totalParticipants = await prisma.participant.count();
-  const totalPayments = await prisma.payment.count();
+  const totalEvents = await prisma.event.count({ where: { organizerId: user.userId } });
+  const totalParticipants = await prisma.participant.count({ where: { userId: user.userId } });
+  const totalRevenue = await prisma.payment.aggregate({
+    _sum: {
+      amount: true
+    },
+    where: {
+      status: PaymentStatus.SUCCESS,
+      userId: user.userId
+    }
+  });
 
   return {
     totalEvents,
     totalParticipants,
-    totalPayments,
+    totalRevenue,
   };
 }
 
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,7 @@ import { authRouter } from "../module/auth/auth.route";
 import { eventRouter } from "../module/event/event.route";
 import { participantRouter } from "../module/participant/participant.route";
 import { reviewRouter } from "../module/review/review.route";
+import { statsRouter } from "../module/stats/stats.route";
 
 const router = express.Router();
 
@@ -11,5 +12,7 @@ router.use("/event", eventRouter)
 router.use("/participant", participantRouter)
 router.use("/review", reviewRouter)
 // router.use("/payment", paymentRouter)
+
+router.use("/stats", statsRouter)
 
 export const IndexRoutes = router;
